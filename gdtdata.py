5cab5eb927e821261ab86226b5662d2d8bf8bf05
100000101 <178890290@qq.com>
2017-07-14 15:21:34 +0800

    gdtdata

--- gdtdata.py
+++ gdtdata.py
@@ -55,34 +55,33 @@ class Test(unittest.TestCase):
             time.sleep(1.0)
         time.sleep(10.0)
         
-        cookie = [item["name"] + "=" + item["value"] for item in driver.get_cookies()]  
-        print cookie    
+        cookie = [item["name"] + "=" + item["value"] for item in driver.get_cookies()]
         cookiestr = ';'.join(item for item in cookie)  
         print cookiestr         
-        driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[3]/div/div/ul/li[1]/span").click()
-        while self.is_element_present(By.XPATH, "/html/body/div[3]/div/div/div[2]/div/ul/li/span") == False:
-            time.sleep(1.0)              
-            driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[3]/div/div/ul/li[1]/span").click()
-        time.sleep(2.0)
-        driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/div/ul/li/span").click()
-        time.sleep(5.0)
-        
-        #媒体
-        driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[3]/div/div/ul/li[2]/span").click()
-        time.sleep(2.0)
-        driver.find_element_by_xpath("/html/body/div[3]/div/div/div[3]/div/ul/li/span").click()
-        driver.find_element_by_link_text(u"查询").click()
-        time.sleep(5.0)
-        driver.find_element_by_link_text(u"下载报告").click()
-        time.sleep(10.0)
-    
+#        driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[3]/div/div/ul/li[1]/span").click()
+#        while self.is_element_present(By.XPATH, "/html/body/div[3]/div/div/div[2]/div/ul/li/span") == False:
+#            time.sleep(1.0)              
+#            driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[3]/div/div/ul/li[1]/span").click()
+#        time.sleep(2.0)
+#        driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/div/ul/li/span").click()
+#        time.sleep(5.0)
+#        
+#        #媒体
+#        driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[3]/div/div/ul/li[2]/span").click()
+#        time.sleep(2.0)
+#        driver.find_element_by_xpath("/html/body/div[3]/div/div/div[3]/div/ul/li/span").click()
+#        driver.find_element_by_link_text(u"查询").click()
+#        time.sleep(5.0)
+#        driver.find_element_by_link_text(u"下载报告").click()
+#        time.sleep(10.0)
+
     #def test_a_(self):
         #self.get_data("2045571685", "Beemans91")
     
     def test_b_(self):
         self.get_data("2772636960@qq.com", "Beemans91")
     
-    def test_c_(self):
+    def atest_c_(self):
         self.get_data("haizon@qq.com", "5x10xngha")
     
     def test_d_umeng(self):
@@ -94,61 +93,65 @@ class Test(unittest.TestCase):
             time.sleep(1.0)
         time.sleep(3.0)
         driver.find_element_by_css_selector("input[type=\"text\"]").clear()
-        driver.find_element_by_css_selector("input[type=\"text\"]").send_keys("178890290@qq.com")
+        driver.find_element_by_css_selector("input[type=\"text\"]").send_keys("fdzclsc@163.com")
         driver.find_element_by_css_selector("input[type=\"password\"]").clear()
-        driver.find_element_by_css_selector("input[type=\"password\"]").send_keys("qawsedrft123")        
+        driver.find_element_by_css_selector("input[type=\"password\"]").send_keys("linshucan090608")
         driver.find_element_by_id("submitForm").click()
         time.sleep(2.0)
         #element = driver.find_element_by_link_text(u"授权应用列表")
         while self.is_element_present(By.LINK_TEXT, u'授权应用列表') == False:
             time.sleep(1.0)
 
-        element = driver.find_element_by_link_text(u"授权应用列表")
-        time.sleep(10.0)
-        element.click()
-        while self.is_element_present(By.XPATH, "//table") == False:
-            time.sleep(1.0)  
-        time.sleep(10.0)
-        driver.find_element_by_link_text(u'在一页内显示').click()
-        time.sleep(30.0)
-        table = driver.find_element_by_xpath("//table")
+        cookie = [item["name"] + "=" + item["value"] for item in driver.get_cookies()]
+        cookiestr = ';'.join(item for item in cookie)
+        print cookiestr
+        
+#        element = driver.find_element_by_link_text(u"授权应用列表")
+#        time.sleep(10.0)
+#        element.click()
+#        while self.is_element_present(By.XPATH, "//table") == False:
+#            time.sleep(1.0)  
+#        time.sleep(10.0)
+#        driver.find_element_by_link_text(u'在一页内显示').click()
+#        time.sleep(30.0)
+#        table = driver.find_element_by_xpath("//table")
+#
+#        localtime = time.localtime(time.time())
+#        filename = time.strftime("%Y_%m_%d", localtime)
+#        workbook = xlsxwriter.Workbook('%s\\%s_umeng.xlsx' %(cert_save_path, filename))
+#        worksheet = workbook.add_worksheet('umeng')        
+#        #插入头部信息
+#        thead = table.find_element_by_xpath(".//thead") 
+#        thead_tr = table.find_element_by_xpath(".//tr")  
+#        thead_th_list = thead_tr.find_elements_by_xpath(".//th")
+#        for i in range(0, len(thead_th_list)):
+#            if i < 1:
+#                continue
+#            worksheet.write(0, i-1, thead_th_list[i].text)
+#        
+#        #插入数据
+#        tbody = table.find_element_by_xpath(".//tbody")
+#        tr_list = tbody.find_elements_by_xpath(".//tr")
+#        
+#        row = 1
+#        for tr in tr_list:
+#            col = 0
+#            td_list = tr.find_elements_by_xpath(".//td")
+#            info = ""
+#            for td in td_list:
+#                #info += td.text + "\t\t"
+#                col += 1
+#                if col < 2:
+#                    continue
+#                text = td.text
+#                if col > 2 and col < 10:
+#                    text = int(td.text.strip())
+#                worksheet.write(row, col - 2, text)
+#            #print info
+#            row += 1
+#        workbook.close()     
 
-        localtime = time.localtime(time.time())
-        filename = time.strftime("%Y_%m_%d", localtime)
-        workbook = xlsxwriter.Workbook('%s\\%s_umeng.xlsx' %(cert_save_path, filename))
-        worksheet = workbook.add_worksheet('umeng')        
-        #插入头部信息
-        thead = table.find_element_by_xpath(".//thead") 
-        thead_tr = table.find_element_by_xpath(".//tr")  
-        thead_th_list = thead_tr.find_elements_by_xpath(".//th")
-        for i in range(0, len(thead_th_list)):
-            if i < 1:
-                continue
-            worksheet.write(0, i-1, thead_th_list[i].text)
-        
-        #插入数据
-        tbody = table.find_element_by_xpath(".//tbody")
-        tr_list = tbody.find_elements_by_xpath(".//tr")
-        
-        row = 1
-        for tr in tr_list:
-            col = 0
-            td_list = tr.find_elements_by_xpath(".//td")
-            info = ""
-            for td in td_list:
-                #info += td.text + "\t\t"
-                col += 1
-                if col < 2:
-                    continue
-                text = td.text
-                if col > 2 and col < 10:
-                    text = int(td.text.strip())
-                worksheet.write(row, col - 2, text)
-            #print info
-            row += 1
-        workbook.close()     
-        
-    def test_z_collect_data(self):
+    def atest_z_collect_data(self):
         statistics_gdt.collect_gdt()
         statistics_gdt.collect_umeng()
         SendEmail.send_email()
@@ -182,7 +185,7 @@ class Test(unittest.TestCase):
     
     def tearDown(self):
         #self.driver.close()
-        #self.driver.quit()
+        self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
